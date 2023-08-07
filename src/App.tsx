c42bc6cb8aac3858a3f869accf5aef5e4205163c
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-08-07 22:23:31 +0300

    Counter_advanced part 1.1

--- src/App.tsx
+++ src/App.tsx
@@ -4,37 +4,81 @@ import './App.css';
 import { Button } from './components/Button/Button';
 import { Counter } from './components/Couter/Couter';
 import s from './App.module.css'
+import { Input } from './components/Input/Input';
 
 
+export const firstMessage = "Enter values and press 'set'"
+export const warningMessage = 'Incorrect value!'
 
-function App() {
+  function App() {
+
+
   let [counter, setCounter] = useState<number>(0)
+  let [maxValue, setMaxValue] = useState<number>(0)
+  let [startValue, setStartValue] = useState<number>(0)
+  let [message, setMessage] = useState<string>('')
+
+
 
   const increaseCounter = () => {
-    if (counter < 5) {
+    if (counter < maxValue) {
       setCounter(counter + 1);
     }
 
   }
 
-
   const resetCounter = () => {
-    setCounter(0)
+    setCounter(startValue)
+  }
+
+  // write in state max value 
+  const changeMaxValue = (value: number) => {
+    
+    setMaxValue(value);
+    value > startValue && value >=0  ? setMessage(firstMessage) : setMessage(warningMessage)
+  }
+
+
+  // write in state start value 
+  const changeStartValue = (value: number) => {
+    setStartValue(value);
+    value < maxValue && value >= 0 ? setMessage(firstMessage) : setMessage(warningMessage)
+  }
+
+
+
+
+  // setting the minimum value
+
+  const settingMinimumValue = () => {
+    setMessage('')
+    setCounter(startValue)
+
   }
 
 
   return (
-    <div className={s.app}>
-      
-      <Counter counter={counter} color={counter < 5 ? 'black' : 'red'} />
-      <div className={s.buttonArea}>
-        <Button name={'INC'} callBack={increaseCounter} color={counter < 5 ? 'aquamarine' : 'grey'} />
-        <Button name={'RESET'} callBack={resetCounter} color={counter > 0 ? 'aquamarine' : 'grey'} />
+    <div>
+
+      <div className={s.app}>
+        <Counter counter={message? message: counter} color={counter < maxValue ? 'black' : 'red'} />
+        <div className={s.buttonArea}>
+          <Button name={'INC'} callBack={increaseCounter} color={counter < maxValue ? 'aquamarine' : 'grey'} />
+          <Button name={'RESET'} callBack={resetCounter} color={counter > 0 ? 'aquamarine' : 'grey'} />
+        </div>
       </div>
-      
-     
+
+      <div className={s.app}>
+        <Input name={'max value'} callBack={changeMaxValue} color={startValue === maxValue || maxValue < 0 ? 'red' : 'black'} />
+        <Input name={'min value'} callBack={changeStartValue} color={startValue === maxValue || startValue < 0 ? 'red' : 'black'} />
+        <div className={s.buttonArea}>
+          <Button name={'SET'} callBack={settingMinimumValue} color={counter === startValue ? 'grey' : 'aquamarine'} />
+        </div>
+      </div>
+
     </div>
   );
 }
 
 export default App;
+
--- src/components/Couter/Couter.tsx
+++ src/components/Couter/Couter.tsx
@@ -1,16 +1,28 @@
 import React from "react";
 import s from './Counter.module.css'
+import { firstMessage, warningMessage } from "../../App";
 
 type PropsCounterType = {
-    counter: number
+    counter: number | string
     color: string
 }
 
 
+
 export const Counter: React.FC<PropsCounterType> = (props) => {
+
+    const styleCounter = `
+    ${props.color === 'black' ? s.active : s.limit}
+    ${props.counter === firstMessage ? s.active : s.limit}
+    ${props.counter === warningMessage ? s.limit : s.active}
+    `
+
     return (
         <div className={s.counter}>
-            <div className={props.color === 'black' ? s.active : s.limit}>{props.counter}</div>
+            <div className={styleCounter}>{props.counter}</div>
         </div>
     )
 } 
+
+
+//props.color === 'black' ? s.active : s.limit
