365b80516d5496a474fd67c398d1f35e372d54a0
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-08-15 19:20:11 +0300

    counterAdvanced_part1

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -5,6 +5,7 @@ type PropsInputType = {
     name: string
     callBack: (value: number) => void
     color: string
+    value: number
 }
 
 
@@ -15,11 +16,13 @@ export const Input: React.FC<PropsInputType> = (props) => {
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         props.callBack(Number(e.currentTarget.value))
     }
-
+    const inputClassName = props.color === 'red' ? `${s.input} ${s.warning}` : s.input
     return (
         <div>
             <span className={s.textValue}>{`${props.name} :`}</span>
-            <span className={s.inputContainer}><input className={props.color === 'red' ? `${s.input} ${s.warning}` : s.input} type={"number"} onChange={onChangeHandler} /></span>
+            <span className={s.inputContainer}>
+                <input value={props.value} className={inputClassName} type={"number"} onChange={onChangeHandler} />
+            </span>
         </div>
 
     )
